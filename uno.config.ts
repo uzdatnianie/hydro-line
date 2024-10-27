3c15348f1438d021496ff8ed3782c35f9ecaad25
Marcin Szafert <mszafert@gmail.com>
2024-10-27 16:11:21 +0100

    ADD google fonts and based theme colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,20 @@
-import { defineConfig, presetUno } from 'unocss';
+import { defineConfig, presetUno, presetWebFonts, transformerDirectives } from 'unocss';
 
 export default defineConfig({
-    presets: [presetUno()],
+    presets: [presetUno(), presetWebFonts({
+        provider: 'google',
+        fonts: {
+            kumbh: {
+                name: 'Kumbh Sans',
+                weights: ['400', '600', '800'],
+            },
+        }
+    })],
+    theme: {
+        colors: {
+            primary: '#ebf7fd',
+            accent: '#7bc3ff',
+        }
+    },
+    transformers: [transformerDirectives()],
 });
