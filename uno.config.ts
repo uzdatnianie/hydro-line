0c76dd797329d159ccd46549457a49aa3d2a46b3
Marcin Szafert <mszafert@gmail.com>
2024-10-28 23:43:08 +0100

    ADD icons & UPDATE footer

--- uno.config.ts
+++ uno.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig, presetUno, presetWebFonts, transformerDirectives } from 'unocss';
+import { defineConfig, presetIcons, presetUno, presetWebFonts, transformerDirectives } from 'unocss';
 
 export default defineConfig({
-    presets: [presetUno(), presetWebFonts({
-        provider: 'google',
-        fonts: {
-            kumbh: {
-                name: 'Kumbh Sans',
-                weights: ['400', '600', '800'],
-            },
-        }
-    })],
+    presets: [
+        presetIcons(),
+        presetUno(),
+        presetWebFonts({
+            provider: 'google',
+            fonts: {
+                kumbh: {
+                    name: 'Kumbh Sans',
+                    weights: ['400', '600', '800'],
+                },
+            }
+        })
+    ],
     rules: [
         ['page-bg', { 'background-image': 'linear-gradient(180deg, #1d242e 0%, #1d2f40 100%)' }],
         ['bluegrad-bg', { 'background-image': 'linear-gradient(90deg, rgba(255,255,255,0.3) 0%, rgba(29,83,128,0.6) 100%)' }],
